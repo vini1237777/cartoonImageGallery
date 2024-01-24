4a7a4f9045e0c332693f71af48d2c5fed1baedeb
vinisha <vinishadv@gmail.com>
2024-01-24 20:52:25 +0530

    minorChange

--- src/components/ItemCard/itemCardStyles.ts
+++ src/components/ItemCard/itemCardStyles.ts
@@ -1,10 +1,10 @@
+import { padding } from "@mui/system";
 import { rem } from "../../utils/functions";
 
 export const styles = {
   container: {
     marginTop: rem(50),
     borderRadius: "10%",
-
   },
   responsiveImage: {
     width: "100%",
@@ -12,8 +12,10 @@ export const styles = {
   },
 
   text: {
-  overflow: 'hidden',
-  whiteSpace: 'nowrap',
-  textOverflow: 'ellipsis'
-}
+    overflow: "hidden",
+    whiteSpace: "nowrap",
+    textOverflow: "ellipsis",
+    paddingLeft: rem(5),
+    paddingRight: rem(5),
+  },
 };

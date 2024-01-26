de1f4d52153d7b8a4362bb944a824199eb9f00e4
vinisha <vinishadv@gmail.com>
2024-01-26 16:10:02 +0530

    minorChange

--- src/components/ItemCard/Image/Image.tsx
+++ src/components/ItemCard/Image/Image.tsx
@@ -14,7 +14,7 @@ const Image = ({ item}: IObject) => {
   return (
     <>
       {!loaded && loaded !== false && 
-        <Skeleton variant="rectangular" width="100%" height={250} sx={{
+        <Skeleton variant="rectangular" width="100%" height={200} sx={{
           position:'relative',
           zIndex:2,
         }} />
